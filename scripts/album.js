07438c832adc04e139acaabbe666cd3a14b8e940
Javier Forero <javierforero01@gmail.com>
2016-07-17 12:44:54 -0500

    not done yet

--- scripts/album.js
+++ scripts/album.js
@@ -60,6 +60,16 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className != targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+    }
+};
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var songRows = document.getElementsByClassName('album-view-song-item');
