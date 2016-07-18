f11313ed114153a90dd7c091c3228d85a22b6540
Javier Forero <javierforero01@gmail.com>
2016-07-17 23:15:42 -0500

    check point 27 not done pt 2

--- scripts/album.js
+++ scripts/album.js
@@ -70,8 +70,41 @@ var findParentByClassName = function(element, targetClass) {
     }
 };
 
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+
+var clickHandler = function(targetElement) {
+    
+    var songItem = getSongItem(targetElement);
+    
+    if(currentlyPlayingSong == null) {
+        songItem.innerHTML = songItem.getAttribute('data-song-number');
+    } else if(currentlyPlayingSong == songItem.getAttribute('data-song-number')) {
+        
+        songItem.innerHTML = playButtonTemplate;
+        currentlyPlayingSong = null;
+    }
+};
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var currentlyPlayingSong = null;
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 window.onload = function() {
@@ -91,6 +124,10 @@ window.onload = function() {
       songRows[i].addEventListener('mouseleave', function(event) {
 
           this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      });
+      
+      songRows[i].addEventListener('click', function(event){
+                   
       });
   }    
 };
