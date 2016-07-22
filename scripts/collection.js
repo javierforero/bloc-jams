7c1cf7d03284876d88b33afb67598c6a45b8c501
Javier Forero <javierforero01@gmail.com>
2016-07-22 08:14:36 -0500

    checkpoint-30 done

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
- var collectionItemTemplate =
+
+ var buildCollectionItemTemplate = function() {
+  var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,13 +15,17 @@
    + '  </div>'
    + '</div>'
    ;
+     
+     return $(template);
+ };
 
-window.onload = function() {
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+$(window).load(function() {
     
-    collectionContainer.innerHTML = '';
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
     
     for (var i = 0; i < 24; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-};
+});
